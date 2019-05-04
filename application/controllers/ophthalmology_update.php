be485405eace25db87d39b8146ee513037bbb74b
Marine <marine.crenn97310@gmail.com>
2019-05-04 20:12:47 +0200

    Ophthalmology

---
+++ application/controllers/ophthalmology_update.php
@@ -0,0 +1,189 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class ophthalmology extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->model('ophthalmology_model');
+        $this->fetch();
+    }
+
+    public function fetch()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('nc5m', '"nc5m"', 'numeric');
+        if ($this->form_validation->run()) {
+            $nc5m = $this->input->post('nc5m');
+            $nc5f = $this->input->post('nc5f');
+            $nc19m = $this->input->post('nc19m');
+            $nc19f = $this->input->post('nc19f');
+            $nc20m = $this->input->post('nc20m');
+            $nc20f = $this->input->post('nc20f');
+
+            $oc5m = $this->input->post('oc5m');
+            $oc5f = $this->input->post('oc5f');
+            $oc19m = $this->input->post('oc19m');
+            $oc19f = $this->input->post('oc19f');
+            $oc20m = $this->input->post('oc20m');
+            $oc20f = $this->input->post('oc20f');
+
+            $insured = $this->input->post('insured');
+            $nc_non_paying = $this->input->post('nc_non_paying');
+            $nc_indigent = $this->input->post('nc_indigent');
+
+            $other_levels = $this->input->post('other_levels');
+            $counter_received = $this->input->post('counter_received');
+
+            $nc_catch = $this->input->post('nc_catch');
+            $nc = $this->input->post('nc');
+            $international = $this->input->post('international');
+
+            $glaucoma_19m = $this->input->post('glaucoma_19m');
+            $glaucoma_19f = $this->input->post('glaucoma_19f');
+            $glaucoma_39m = $this->input->post('glaucoma_39m');
+            $glaucoma_39f = $this->input->post('glaucoma_39f');
+            $glaucoma_40m = $this->input->post('glaucoma_40m');
+            $glaucoma_40f = $this->input->post('glaucoma_40f');
+
+            $cataract_19m = $this->input->post('cataract_19m');
+            $cataract_19f = $this->input->post('cataract_19f');
+            $cataract_39m = $this->input->post('cataract_39m');
+            $cataract_39f = $this->input->post('cataract_39f');
+            $cataract_40m = $this->input->post('cataract_40m');
+            $cataract_40f = $this->input->post('cataract_40f');
+
+            $refrac_error_19m = $this->input->post('refrac_error_19m');
+            $refrac_error_19f = $this->input->post('refrac_error_19f');
+            $refrac_error_39m = $this->input->post('refrac_error_39m');
+            $refrac_error_39f = $this->input->post('refrac_error_39f');
+            $refrac_error_40m = $this->input->post('refrac_error_40m');
+            $refrac_error_40f = $this->input->post('refrac_error_40f');
+
+            $diab_retino_19m = $this->input->post('diab_retino_19m');
+            $diab_retino_19f = $this->input->post('diab_retino_19f');
+            $diab_retino_39m = $this->input->post('diab_retino_39m');
+            $diab_retino_39f = $this->input->post('diab_retino_39f');
+            $diab_retino_40m = $this->input->post('diab_retino_40m');
+            $diab_retino_40f = $this->input->post('diab_retino_40f');
+
+            $conjunctivitis_19m = $this->input->post('conjunctivitis_19m');
+            $conjunctivitis_19f = $this->input->post('conjunctivitis_19f');
+            $conjunctivitis_39m = $this->input->post('conjunctivitis_39m');
+            $conjunctivitis_39f = $this->input->post('conjunctivitis_39f');
+            $conjunctivitis_40m = $this->input->post('conjunctivitis_40m');
+            $conjunctivitis_40f = $this->input->post('conjunctivitis_40f');
+
+            $pinguecula_19m = $this->input->post('pinguecula_19m');
+            $pinguecula_19f = $this->input->post('pinguecula_19f');
+            $pinguecula_39m = $this->input->post('pinguecula_39m');
+            $pinguecula_39f = $this->input->post('pinguecula_39f');
+            $pinguecula_40m = $this->input->post('pinguecula_40m');
+            $pinguecula_40f = $this->input->post('pinguecula_40f');
+
+            $uveitis_19m = $this->input->post('uveitis_19m');
+            $uveitis_19f = $this->input->post('uveitis_19f');
+            $uveitis_39m = $this->input->post('uveitis_39m');
+            $uveitis_39f = $this->input->post('uveitis_39f');
+            $uveitis_40m = $this->input->post('uveitis_40m');
+            $uveitis_40f = $this->input->post('uveitis_40f');
+
+            $eye_trauma_19m = $this->input->post('eye_trauma_19m');
+            $eye_trauma_19f = $this->input->post('eye_trauma_19f');
+            $eye_trauma_39m = $this->input->post('eye_trauma_39m');
+            $eye_trauma_39f = $this->input->post('eye_trauma_39f');
+            $eye_trauma_40m = $this->input->post('eye_trauma_40m');
+            $eye_trauma_40f = $this->input->post('eye_trauma_40f');
+
+            $eye_pb_19m = $this->input->post('eye_pb_19m');
+            $eye_pb_19f = $this->input->post('eye_pb_19f');
+            $eye_pb_39m = $this->input->post('eye_pb_39m');
+            $eye_pb_39f = $this->input->post('eye_pb_39f');
+            $eye_pb_40m = $this->input->post('eye_pb_40m');
+            $eye_pb_40f = $this->input->post('eye_pb_40f');
+
+            $this->ophthalmology_model->update_data1(
+          $nc5m,
+            $nc5f,
+            $nc19f,
+            $nc19m,
+          $nc20m,
+            $nc20f,
+          $oc5m,
+            $oc5f,
+            $oc19f,
+            $oc19m,
+          $oc20m,
+            $oc20f,
+          $insured,
+            $nc_non_paying,
+            $nc_indigent,
+          $other_levels,
+            $counter_received,
+          $nc_catch,
+            $nc,
+            $international
+        );
+                    $this->ophthalmology_model->update_data2(
+                  $glaucoma_19f,
+                    $glaucoma_19m,
+                    $glaucoma_39m,
+                    $glaucoma_39f,
+                    $glaucoma_40m,
+                    $glaucoma_40f,
+                  $cataract_19f,
+                    $cataract_19m,
+                    $cataract_39m,
+                    $cataract_39f,
+                    $cataract_40m,
+                    $cataract_40f,
+                  $refrac_error_19f,
+                    $refrac_error_19m,
+                    $refrac_error_39m,
+                    $refrac_error_39f,
+                    $refrac_error_40m,
+                    $refrac_error_40f,
+                  $diab_retino_19f,
+                    $diab_retino_19m,
+                    $diab_retino_39m,
+                    $diab_retino_39f,
+                    $diab_retino_40m,
+                    $diab_retino_40f,
+                  $conjunctivitis_19f,
+                    $conjunctivitis_19m,
+                    $conjunctivitis_39m,
+                    $conjunctivitis_39f,
+                    $conjunctivitis_40m,
+                    $conjunctivitis_40f,
+                  $pinguecula_19f,
+                    $pinguecula_19m,
+                    $pinguecula_39m,
+                    $pinguecula_39f,
+                    $pinguecula_40m,
+                    $pinguecula_40f,
+                  $uveitis_19f,
+                    $uveitis_19m,
+                    $uveitis_39m,
+                    $uveitis_39f,
+                    $uveitis_40m,
+                    $uveitis_40f,
+                  $eye_trauma_19m,
+                    $eye_trauma_19f,
+                    $eye_trauma_39m,
+                    $eye_trauma_39f,
+                    $eye_trauma_40m,
+                    $eye_trauma_40f,
+                  $eye_pb_19m,
+                    $eye_pb_19f,
+                    $eye_pb_39f,
+                    $eye_pb_39m,
+                    $eye_pb_40m,
+                    $eye_pb_40f
+                );
+        } else {
+          $data['table1'] = $this->ophthalmology_model->show_table1();
+          $data['table2'] = $this->ophthalmology_model->show_table2();
+            $this->load->view('ophthalmology_update_view',$data);
+        }
+    }
+}
---
+++ application/views/ophthalmology_update_view.php
@@ -0,0 +1,273 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Ophthalmology</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:400,500|Roboto:300,400,500,700">
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/recherche14.css'); ?>" />
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css">
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	  <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+	</head>
+	<body>
+		<div class="container-fluid">
+      <div>
+        <form method="post" action="" class="form-inline">
+      <h2> A) Outpatient morbidity summary table </h2>
+      <table id="A">
+        <thead>
+          <tr>
+            <th rowspan="2" class="des">Outpatient visits<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+            <th colspan="2">Age < 5<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+            <th colspan="2">Age 5 to 19<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+            <th colspan="2">Age > 20<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+          </tr>
+          <tr>
+            <th>M</th>
+            <th>F</th>
+            <th>M</th>
+            <th>F</th>
+            <th>M</th>
+            <th>F</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>New cases (NC)</td>
+            <td><input type="number" min="0" name="nc5m" class="form-control" value="<?php echo $table1->nc5m; ?>"></td>
+            <td><input type="number" min="0" name="nc5f" class="form-control" value="<?php echo $table1->nc5f; ?>"></td>
+            <td><input type="number" min="0" name="nc19m" class="form-control" value="<?php echo $table1->nc19m; ?>"></td>
+            <td><input type="number" min="0" name="nc19f" class="form-control" value="<?php echo $table1->nc19f; ?>"></td>
+            <td><input type="number" min="0" name="nc20m" class="form-control" value="<?php echo $table1->nc20m; ?>"></td>
+            <td><input type="number" min="0" name="nc20f" class="form-control" value="<?php echo $table1->nc20f; ?>"></td>
+          </tr>
+          <tr>
+            <td>Old cases</td>
+            <td><input type="number" min="0" name="oc5m" class="form-control" value="<?php echo $table1->oc5m; ?>"></td>
+            <td><input type="number" min="0" name="oc5f" class="form-control" value="<?php echo $table1->oc5f; ?>"></td>
+            <td><input type="number" min="0" name="oc19m" class="form-control" value="<?php echo $table1->oc19m; ?>"></td>
+            <td><input type="number" min="0" name="oc19f" class="form-control" value="<?php echo $table1->oc19f; ?>"></td>
+            <td><input type="number" min="0" name="oc20m" class="form-control" value="<?php echo $table1->oc20m; ?>"></td>
+            <td><input type="number" min="0" name="oc20f" class="form-control" value="<?php echo $table1->oc20f; ?>"></td>
+          </tr>
+        </tbody>
+      </table>
+      <h2> B) Health insurance status of new cases </h2>
+      <table id="B">
+        <thead>
+          <tr>
+            <th class="des">Designation<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+            <th >Total<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Insured (Mutuelle or other insurance members)</td>
+            <td><input type="number" min="0" name="insured" class="form-control"  value="<?php echo $table1->insured; ?>"></td>
+          </tr>
+          <tr>
+            <td>Non-paying new cases</td>
+            <td><input type="number" min="0" name="nc_non_paying" class="form-control" value="<?php echo $table1->nc_non_paying; ?>"></td>
+          </tr>
+          <tr>
+            <td>Number of indigent new cases</td>
+            <td><input type="number" min="0" name="nc_indigent" class="form-control" value="<?php echo $table1->nc_indigent; ?>"></td>
+          </tr>
+        </tbody>
+      </table>
+      <h2> C) Referrals </h2>
+      <table id="C">
+        <thead>
+          <tr>
+            <th class="des">Designation<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+            <th >Total<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Referred to other levels</td>
+            <td><input type="number" min="0" name="other_levels" class="form-control"  value="<?php echo $table1->other_levels; ?>"></td>
+          </tr>
+          <tr>
+            <td>Counter referrals received</td>
+            <td><input type="number" min="0" name="counter_received
+
+              " class="form-control" value="<?php echo $table1->counter_received; ?>"></td>
+          </tr>
+        </tbody>
+      </table>
+        <h2> D) Origin of outpatients </h2>
+      <table id="D">
+        <thead>
+          <tr>
+            <th class="des">Designation<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+            <th >Total<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>New cases from the catchment area (zone)</td>
+            <td><input type="number" min="0" name="nc_catch" class="form-control"  value="<?php echo $table1->nc_catch; ?>"></td>
+          </tr>
+          <tr>
+            <td>New cases (hors zone)</td>
+            <td><input type="number" min="0" name="nc" class="form-control" value="<?php echo $table1->nc; ?>"></td>
+          </tr>
+          <tr>
+            <td>International patients (hors pays)</td>
+            <td><input type="number" min="0" name="international" class="form-control" value="<?php echo $table1->international; ?>"></td>
+          </tr>
+        </tbody>
+      </table>
+      <h2> Ga) Eye diseases </h2>
+      <table id="Ga">
+        <thead>
+          <tr>
+            <th rowspan="2">N°<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+            <th rowspan="2" class="des">Designation<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+            <th colspan="2">Age < 19<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+            <th colspan="2">Age 20 to 39<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+            <th colspan="2">Age > 40<i class="fa fa-sort float-right" aria-hidden="true"></i></th>
+          </tr>
+          <tr>
+            <th>M</th>
+            <th>F</th>
+            <th>M</th>
+            <th>F</th>
+            <th>M</th>
+            <th>F</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="center">1</td>
+            <td>Glaucoma</td>
+            <td><input type="number" min="0" name="glaucoma_19m" class="form-control" value="<?php echo $table2->glaucoma_19m; ?>"></td>
+            <td><input type="number" min="0" name="glaucoma_19f" class="form-control" value="<?php echo $table2->glaucoma_19f; ?>"></td>
+            <td><input type="number" min="0" name="glaucoma_39m" class="form-control" value="<?php echo $table2->glaucoma_39m; ?>"></td>
+            <td><input type="number" min="0" name="glaucoma_39f" class="form-control" value="<?php echo $table2->glaucoma_39f; ?>"></td>
+            <td><input type="number" min="0" name="glaucoma_40m" class="form-control" value="<?php echo $table2->glaucoma_40m; ?>"></td>
+            <td><input type="number" min="0" name="glaucoma_40f" class="form-control" value="<?php echo $table2->glaucoma_40f; ?>"></td>
+          </tr>
+          <tr>
+            <td class="center">2</td>
+            <td>Cataract</td>
+            <td><input type="number" min="0" name="cataract_19m" class="form-control" value="<?php echo $table2->cataract_19m; ?>"></td>
+            <td><input type="number" min="0" name="cataract_19f" class="form-control" value="<?php echo $table2->cataract_19f; ?>"></td>
+            <td><input type="number" min="0" name="cataract_39m" class="form-control" value="<?php echo $table2->cataract_39m; ?>"></td>
+            <td><input type="number" min="0" name="cataract_39f" class="form-control" value="<?php echo $table2->cataract_39f; ?>"></td>
+            <td><input type="number" min="0" name="cataract_40m" class="form-control" value="<?php echo $table2->cataract_40m; ?>"></td>
+            <td><input type="number" min="0" name="cataract_40f" class="form-control" value="<?php echo $table2->cataract_40f; ?>"></td>
+          </tr>
+          <tr>
+            <td class="center">3</td>
+            <td>Refractive error</td>
+            <td><input type="number" min="0" name="refrac_error_19m" class="form-control" value="<?php echo $table2->refrac_error_19m; ?>"></td>
+            <td><input type="number" min="0" name="refrac_error_19f" class="form-control" value="<?php echo $table2->refrac_error_19f; ?>"></td>
+            <td><input type="number" min="0" name="refrac_error_39m" class="form-control" value="<?php echo $table2->refrac_error_39m; ?>"></td>
+            <td><input type="number" min="0" name="refrac_error_39f" class="form-control" value="<?php echo $table2->refrac_error_39f; ?>"></td>
+            <td><input type="number" min="0" name="refrac_error_40m" class="form-control" value="<?php echo $table2->refrac_error_40m; ?>"></td>
+            <td><input type="number" min="0" name="refrac_error_40f" class="form-control" value="<?php echo $table2->refrac_error_40f; ?>"></td>
+          </tr>
+          <tr>
+            <td class="center">4</td>
+            <td>Diabetic retinopathy</td>
+            <td><input type="number" min="0" name="diab_retino_19m" class="form-control" value="<?php echo $table2->diab_retino_19m; ?>"></td>
+            <td><input type="number" min="0" name="diab_retino_19f" class="form-control" value="<?php echo $table2->diab_retino_19f; ?>"></td>
+            <td><input type="number" min="0" name="diab_retino_39m" class="form-control" value="<?php echo $table2->diab_retino_39m; ?>"></td>
+            <td><input type="number" min="0" name="diab_retino_39f" class="form-control" value="<?php echo $table2->diab_retino_39f; ?>"></td>
+            <td><input type="number" min="0" name="diab_retino_40m" class="form-control" value="<?php echo $table2->diab_retino_40m; ?>"></td>
+            <td><input type="number" min="0" name="diab_retino_40f" class="form-control" value="<?php echo $table2->diab_retino_40f; ?>"></td>
+          </tr>
+          <tr>
+            <td class="center">5</td>
+            <td>Conjunctivitis</td>
+            <td><input type="number" min="0" name="conjunctivitis_19m" class="form-control" value="<?php echo $table2->conjunctivitis_19m; ?>"></td>
+            <td><input type="number" min="0" name="conjunctivitis_19f" class="form-control" value="<?php echo $table2->conjunctivitis_19f; ?>"></td>
+            <td><input type="number" min="0" name="conjunctivitis_39m" class="form-control" value="<?php echo $table2->conjunctivitis_39m; ?>"></td>
+            <td><input type="number" min="0" name="conjunctivitis_39f" class="form-control" value="<?php echo $table2->conjunctivitis_39f; ?>"></td>
+            <td><input type="number" min="0" name="conjunctivitis_40m" class="form-control" value="<?php echo $table2->conjunctivitis_40m; ?>"></td>
+            <td><input type="number" min="0" name="conjunctivitis_40f" class="form-control" value="<?php echo $table2->conjunctivitis_40f; ?>"></td>
+          </tr>
+          <tr>
+            <td class="center">6</td>
+            <td>Pinguecula</td>
+            <td><input type="number" min="0" name="pinguela_19m" class="form-control" value="<?php echo $table2->pinguela_19m; ?>"></td>
+            <td><input type="number" min="0" name="pinguela_19f" class="form-control" value="<?php echo $table2->pinguela_19f; ?>"></td>
+            <td><input type="number" min="0" name="pinguela_39m" class="form-control" value="<?php echo $table2->pinguela_39m; ?>"></td>
+            <td><input type="number" min="0" name="pinguela_39f" class="form-control" value="<?php echo $table2->pinguela_39f; ?>"></td>
+            <td><input type="number" min="0" name="pinguela_40m" class="form-control" value="<?php echo $table2->pinguela_40m; ?>"></td>
+            <td><input type="number" min="0" name="pinguela_40f" class="form-control" value="<?php echo $table2->pinguela_40f; ?>"></td>
+          </tr>
+          <tr>
+            <td class="center">7</td>
+            <td>Uveitis</td>
+            <td><input type="number" min="0" name="uveitis_19m" class="form-control" value="<?php echo $table2->uveitis_19m; ?>"></td>
+            <td><input type="number" min="0" name="uveitis_19f" class="form-control" value="<?php echo $table2->uveitis_19f; ?>"></td>
+            <td><input type="number" min="0" name="uveitis_39m" class="form-control" value="<?php echo $table2->uveitis_39m; ?>"></td>
+            <td><input type="number" min="0" name="uveitis_39f" class="form-control" value="<?php echo $table2->uveitis_39f; ?>"></td>
+            <td><input type="number" min="0" name="uveitis_40m" class="form-control" value="<?php echo $table2->uveitis_40m; ?>"></td>
+            <td><input type="number" min="0" name="uveitis_40f" class="form-control" value="<?php echo $table2->uveitis_40f; ?>"></td>
+          </tr>
+          <tr>
+            <td class="center">8</td>
+            <td>Eye trauma</td>
+            <td><input type="number" min="0" name="eye_trauma_19m" class="form-control" value="<?php echo $table2->eye_trauma_19m; ?>"></td>
+            <td><input type="number" min="0" name="eye_trauma_19f" class="form-control" value="<?php echo $table2->eye_trauma_19f; ?>"></td>
+            <td><input type="number" min="0" name="eye_trauma_39m" class="form-control" value="<?php echo $table2->eye_trauma_39m; ?>"></td>
+            <td><input type="number" min="0" name="eye_trauma_39f" class="form-control" value="<?php echo $table2->eye_trauma_39f; ?>"></td>
+            <td><input type="number" min="0" name="eye_trauma_40m" class="form-control" value="<?php echo $table2->eye_trauma_40m; ?>"></td>
+            <td><input type="number" min="0" name="eye_trauma_40f" class="form-control" value="<?php echo $table2->eye_trauma_40f; ?>"></td>
+          </tr>
+          <tr>
+            <td class="center">9</td>
+            <td>Eye problem other</td>
+            <td><input type="number" min="0" name="eye_pb_19m" class="form-control" value="<?php echo $table2->eye_pb_19m; ?>"></td>
+            <td><input type="number" min="0" name="eye_pb_19f" class="form-control" value="<?php echo $table2->eye_pb_19f; ?>"></td>
+            <td><input type="number" min="0" name="eye_pb_39m" class="form-control" value="<?php echo $table2->eye_pb_39m; ?>"></td>
+            <td><input type="number" min="0" name="eye_pb_39f" class="form-control" value="<?php echo $table2->eye_pb_39f; ?>"></td>
+            <td><input type="number" min="0" name="eye_pb_40m" class="form-control" value="<?php echo $table2->eye_pb_40m; ?>"></td>
+            <td><input type="number" min="0" name="eye_pb_40f" class="form-control" value="<?php echo $table2->eye_pb_40f; ?>"></td>
+          </tr>
+        </tbody>
+      </table>
+      <center><button type="submit">Validate</button></center>
+    </form>
+    </div>
+
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
+		<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+  </body>
+</html>
+<script>
+$('#A').DataTable({
+        "aLengthMenu": [[25, 50, 75, -1], [25, 50, 75, "All"]],
+        "iDisplayLength": 25
+    });
+$('#B').DataTable({
+        "aLengthMenu": [[25, 50, 75, -1], [25, 50, 75, "All"]],
+        "iDisplayLength": 25
+    });
+$('#C').DataTable({
+        "aLengthMenu": [[25, 50, 75, -1], [25, 50, 75, "All"]],
+        "iDisplayLength": 25
+    });
+$('#D').DataTable({
+        "aLengthMenu": [[25, 50, 75, -1], [25, 50, 75, "All"]],
+        "iDisplayLength": 25
+    });
+$('#Ga').DataTable({
+        "aLengthMenu": [[25, 50, 75, -1], [25, 50, 75, "All"]],
+        "iDisplayLength": 25
+    });
+$('.dataTables_length').addClass('bs-select');
+</script>
